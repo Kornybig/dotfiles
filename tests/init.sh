0a9849a93dcfbe79405afe06fd3008994797b911
Olle Emilsson <olle@emilsson.io>
2020-04-22 15:50:22 +0200

    Bugfixes...

--- tests/init.sh
+++ tests/init.sh
@@ -16,8 +16,16 @@ SNAPSHOT=$(VBoxManage snapshot $VM list | tail -n 1 | grep -o '\([0-9a-z]*-\)\{4
 VBoxManage snapshot $VM restore $SNAPSHOT
 VBoxManage startvm $VM --type headless
 
-# BECAUSE COMPUTER IZ 2 TRIGGER HAPPY FOR DEBIAN
-sleep 10
+# BECAUSE COMPUTER IS 2 TRIGGER HAPPY FOR DEBIAN
+sleep 15
+
+if [ $os == 'debian' ]; then
+    # Sync time because snapshot is wierd and dont want to sync correctly
+    # TODO: Create new snapshot where prompt for sudo is no need
+    HOST=$(cat $HOME/.dotfiles/ansible/hosts.ini | grep debian | grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}')
+    sshpass -p Password!1234 ssh -t olle@$HOST "echo Password!1234 | sudo -S chronyd -q 'server pool.ntp.org iburst'"
+fi
+
 
 ansible-playbook $HOME/.dotfiles/ansible/dotfiles.yaml \
     -i $HOME/.dotfiles/ansible/hosts.ini \
